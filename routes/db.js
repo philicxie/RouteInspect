06fc40efdc24fd5ab97f500bf400fb5812e6bc8f
Philic <xlw125414713@163.com>
2017-03-31 11:07:01 +0800

    code beatuified

--- routes/db.js
+++ routes/db.js
@@ -5,35 +5,37 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/RouteInspect');
 
 var userSchema = mongoose.Schema({
-    account: String,
-    name: String,
-    password: String,
-    auth: Number
+    account     :       String,
+    name        :       String,
+    password    :       String,
+    auth        :       Number
 });
 
 var facilitySchema = mongoose.Schema({
-    name: String,
-    uid: String,
-    area: String,
-    address: String,
-    position: [Number],
-    status: String,
-    category: String
+    name        :       String,
+    uid         :       String,
+    area        :       String,
+    address     :       String,
+    position    :       [Number],
+    status      :       String,
+    category    :       String
 });
 
 var missionSchema = mongoose.Schema({
-    uid: String,
-    facility: [String],
-    status: String,
-    category: String,
-    date: Date,
-    time: Number,
-    manager: String,
-    worker: [String],
-    abstract: String,
-    notes: [String]
+    uid         :       String,
+    facility    :       [String],
+    status      :       String,
+    category    :       String,
+    date        :       Date,
+    time        :       Number,
+    manager     :       String,
+    worker      :       [String],
+    abstract    :       String,
+    notes       :       [String]
 });
 
-module.exports.user = mongoose.model('Users', userSchema);
+module.exports.user     = mongoose.model('Users',       userSchema      );
 
-module.exports.facility = mongoose.model('Facilities', facilitySchema);
+module.exports.facility = mongoose.model('Facilities',  facilitySchema  );
+
+module.exports.mission  = mongoose.model('Missions',    missionSchema   );
