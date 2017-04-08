dc6150b0a854712f8e642d1ac1d3d51e710414ee
Philic <xlw125414713@163.com>
2017-04-08 15:50:54 +0800

    createMission logic init

--- routes/missionRemote.js
+++ routes/missionRemote.js
@@ -45,11 +45,30 @@ router.post('/getAllFacility', function(req, res, next) {
     Facility.find(function(err, doc) {
         if(err) return console.error(err);
         res.send(doc);
-    })
+    });
 });
 
 router.post('/createMission', function(req, res, next) {
-
+    // var mission = new Mission({uid: 1000});
+    // mission.save();
+    // res.send(200);
+    var sortDoc = function(a, b) {
+        return a.uid > b.uid;
+    };
+    Mission.find(function(err, doc) {
+        if(err) {
+            return console.error(err);
+        }
+        console.log(doc);
+        doc.sort(sortDoc);
+        var initId = doc[0].uid*1+1;
+        var initMission = new Mission({
+            uid: initId,
+            status: 0
+        });
+        initMission.save();
+        res.send(initId);
+    });
 });
 
 router.post('/commitMission', function(req, res, next) {
